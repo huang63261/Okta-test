1024066c9d1cf77e447e95fa1502f0ea069feba7
Harvey Huang <harvey.huang@staff.nueip.com>
2024-01-09 11:39:50 +0800

    Add: PostController, Post Model, PostFactory

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Throwable;
+
+class PostController extends Controller
+{
+    //用於生成 JSON 字串
+    private function makeJson($status, $data, $msg)
+    {
+        //轉 JSON 時確保中文不會變成 Unicode
+        return response()->json(['status' => $status, 'data' => $data, 'message' => $msg])->setEncodingOptions(JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::get();
+
+        if(isset($posts) && count($posts) > 0){
+            $data = ['posts' => $posts];
+            return $this->makeJson(1,$data,null);
+        }else{
+            return $this->makeJson(0,null,'找不到任何文章');
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = ['title' => $request->title , 'content' => $request->content];
+
+        $post = Post::create($input);
+
+        if(isset($post)){
+            $data = ['post' => $post];
+            return $this->makeJson(1,$data,'新增文章成功');
+        }else{
+            $data = ['post' => $post];
+            return $this->makeJson(0,null,'新增文章失敗');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::find($id);
+
+        if(isset($post)){
+            $data = ['post' => $post];
+            return $this->makeJson(1,$data,null);
+        }else{
+            return $this->makeJson(0,null,'找不到該文章');
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            $post->title = $request->title;
+            $post->content = $request->content;
+            $post->save();
+        } catch (Throwable $e) {
+            //更新失敗
+            $data = ['post' => $post];
+            return $this->makeJson(0,null,'更新文章失敗');
+        }
+
+        $data = ['post' => $post];
+        return $this->makeJson(1,$data,'更新文章成功');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            $post->delete();
+        } catch (Throwable $e) {
+            //刪除失敗
+            return $this->makeJson(0,null,'刪除文章失敗');
+        }
+        return $this->makeJson(1,null,'刪除文章成功');
+    }
+}
